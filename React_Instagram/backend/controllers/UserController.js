1c552c43114db63b511ff5260aa965cb8193fb50
Raekwon-Onirele <contato.raekwon@gmail.com>
2026-04-22 16:49:59 -0300

    Alteração Controler e Routes
    tá dando erro em algum lugar

--- React_Instagram/backend/controllers/UserController.js
+++ React_Instagram/backend/controllers/UserController.js
@@ -4,3 +4,19 @@ const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 const jwtSecret = process.env.JWT_SECRET
+
+// Gerador de User Token
+const generateToken = (id) => {
+    return jwt.sign({id}, jwtSecret, {
+        expiresIn: "7d",
+    })
+}
+
+// Register User e sign in
+const register = async (req, res) => {
+    res.send("Registro")
+}
+
+module.exports = {
+    register
+}
