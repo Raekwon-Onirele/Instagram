e7ec8f1b62fdcc74c61c6f0c88dc6f4a6ec8d153
Raekwon-Onirele <contato.raekwon@gmail.com>
2026-05-01 16:30:20 -0300

    Create Update user

--- React_Instagram/backend/controllers/UserController.js
+++ React_Instagram/backend/controllers/UserController.js
@@ -5,6 +5,8 @@ const jwt = require("jsonwebtoken");
 
 const jwtSecret = process.env.JWT_SECRET;
 
+const mongoose = require("mongoose")
+
 // Gerador de User Token
 const generateToken = (id) => {
   return jwt.sign({ id }, jwtSecret, {
@@ -61,8 +63,8 @@ const login = async (req, res) => {
 
   // Checando se as senhas são iguais
   if (!(await bcrypt.compare(password, user.password))) {
-    res.status(422).json({errors: ["Senha inválida"]})
-    return
+    res.status(422).json({ errors: ["Senha inválida"] });
+    return;
   }
 
   // Retornando User com token
@@ -74,16 +76,53 @@ const login = async (req, res) => {
 };
 
 // Get para o user logado
-const getCurrentUser = async(req, res) => {
-    const user = req.user
+const getCurrentUser = async (req, res) => {
+  const user = req.user;
 
-    res.status(200).json(user)
-}
+  res.status(200).json(user);
+};
 
 // Update User
 const update = async (req, res) => {
-  res.send("Update")
-}
+
+  const {name, password, bio} = req.body;
+
+  let profileImage = null;
+
+  if (req.file) {
+    profileImage = req.file.filename;
+  }
+
+  const reqUser = req.user;
+
+  const user = await User.findById(new mongoose.Types.ObjectId(reqUser._id)).select(
+    "-password",
+);
+
+  if (name) {
+    user.name = name;
+  }
+
+  if (password) {
+    // Gerar password hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    user.password = passwordHash
+  }
+
+  if(profileImage) {
+    user.profileImage = profileImage
+  }
+
+  if(bio) {
+    user.bio = bio
+  }
+
+  await user.save()
+
+  res.status(200).json(user)
+};
 
 module.exports = {
   register,
