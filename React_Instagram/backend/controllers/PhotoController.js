fe92a46b88e329ec60b590dad2fa785abfd00fdb
Raekwon-Onirele <contato.raekwon@gmail.com>
2026-05-11 15:40:05 -0300

    Finished backend

--- React_Instagram/backend/controllers/PhotoController.js
+++ React_Instagram/backend/controllers/PhotoController.js
@@ -62,76 +62,145 @@ const deletePhoto = async (req, res) => {
 };
 
 // Get de todas as photos
-const getAllPhotos = async(req, res) => {
-    const photos = await Photo.find({})
-        .sort([["createdAt", -1]])
-        .exec()
+const getAllPhotos = async (req, res) => {
+  const photos = await Photo.find({})
+    .sort([["createdAt", -1]])
+    .exec();
 
-    return res.status(200).json(photos)
-}
+  return res.status(200).json(photos);
+};
 
 // get na photo do user
-const getUserPhotos = async(req, res) => {
+const getUserPhotos = async (req, res) => {
+  const { id } = req.params;
 
-    const {id} = req.params
-
-    const photos = await Photo.find({userId: id})
+  const photos = await Photo.find({ userId: id })
     .sort([["createdAt", -1]])
-    .exec()
+    .exec();
 
-    return res.status(200).json(photos)
-}
+  return res.status(200).json(photos);
+};
 
 const getPhotoById = async (req, res) => {
+  const { id } = req.params;
 
-    const {id} = req.params
+  const photo = await Photo.findById(new mongoose.Types.ObjectId(id));
 
-    const photo = await Photo.findById(new mongoose.Types.ObjectId(id))
+  // Checagem se a foto existe
+  if (!photo) {
+    res.status(404).json({ errors: [" Foto não encontrada "] });
+    return;
+  }
 
-    // Checagem se a foto existe
-    if (!photo) {
-        res.status(404).json({errors: [" Foto não encontrada "]})
-        return
-    }
-    
-    res.status(200).json(photo)
-}
+  res.status(200).json(photo);
+};
 
 // Update Photo
 const updatePhoto = async (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
 
-    const {id} = req.params;
-    const {title} = req.body;
+  const reqUser = req.user;
 
-    const reqUser = req.user
+  const photo = await Photo.findById(id);
 
-    const photo = await Photo.findById(id)
+  // Checagem se a foto existe
+  if (!photo) {
+    res.status(404).json({ errors: [" Foto não encontrada "] });
+    return;
+  }
 
-    // Checagem se a foto existe
-    if (!photo) {
-        res.status(404).json({errors: [" Foto não encontrada "]})
-        return
-    }
+  // Checagem se foto pertence ao User
+  if (!photo.userId.equals(reqUser._id)) {
+    res.status(422).json({
+      errors: ["Ocorreu um erro, por favor tente mais tarde"],
+    });
+    return;
+  }
 
-    // Checagem se foto pertence ao User
-    if(!photo.userId.equals(reqUser._id)) {
-        res.status(422).json({
-            errors: ["Ocorreu um erro, por favor tente mais tarde"]
-        })
-        return 
-    }
+  if (title) {
+    photo.title = title;
+  }
 
-    if(title) {
-        photo.title = title
-    }
+  await photo.save();
 
-    await photo.save()
-
-    res.status(200).json({photo, message: "Foto atualizada com sucesso"})
-
-}
+  res.status(200).json({ photo, message: "Foto atualizada com sucesso" });
+};
 
 // Funcionalidade Like
+const likePhoto = async (req, res) => {
+  const { id } = req.params;
+
+  const reqUser = req.user;
+
+  const photo = await Photo.findById(id);
+
+  // Checagem se a foto existe
+  if (!photo) {
+    res.status(404).json({ errors: [" Foto não encontrada "] });
+    return;
+  }
+
+  // Checagem se o User já deu like
+  if (photo.likes.includes(reqUser._id)) {
+    res.status(422).json({ errors: ["Você já curtiu a foto"] });
+    return;
+  }
+
+  photo.likes.push(reqUser._id);
+
+  photo.save();
+
+  res
+    .status(200)
+    .json({ photo: id, userId: reqUser._id, message: "A foto foi curtida" });
+};
+
+// Funcionalidade comentários
+const commentPhoto = async (req, res) => {
+  const { id } = req.params;
+  const { comment } = req.body;
+
+  const reqUser = req.user;
+
+  const user = await User.findById(reqUser._id);
+
+  const photo = await Photo.findById(id);
+
+  // Checagem se a foto existe
+  if (!photo) {
+    res.status(404).json({ errors: [" Foto não encontrada "] });
+    return;
+  }
+
+  // put do comentário dentro do array comments
+  const userComment = {
+    comment,
+    userName: user.name,
+    userImage: user.profileImage,
+    userId: user._id,
+  };
+
+  photo.comments.push(userComment);
+
+  await photo.save();
+
+  res.status(200).json({
+    comment: userComment,
+    message: "O comentário foi inserido com sucesso!",
+  });
+};
+
+// Procurar fotos pelo titulo
+const searchPhotos = async (req, res) => {
+
+  const {q} = req.query
+
+  const photos = await Photo.find({title: new RegExp(q, "i")}).exec()
+
+  res.status(200).json(photos)
+
+}
 
 module.exports = {
   insertPhoto,
@@ -140,4 +209,7 @@ module.exports = {
   getUserPhotos,
   getPhotoById,
   updatePhoto,
+  likePhoto,
+  commentPhoto,
+  searchPhotos
 };
